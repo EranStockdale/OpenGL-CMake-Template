828f207f2222b0faaefacfcc6acaee66c55067f2
Eran Stockdale <eranbd2017@outlook.com>
2023-07-11 22:20:11 +0100

    start the triangle code but I have to go to sleep now

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,18 @@ int main(int, char**){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // triangle go brrr
+        float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f , -0.5f, 0.0f,
+            0.0f ,  0.5f, 0.0f
+        }; 
+
+        unsigned int VBO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 
         /// ---= STUFF =---
         // swap buffers
