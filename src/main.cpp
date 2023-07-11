06548e89daa83dfb357a71fdd8fea84c5560740f
Eran Stockdale <eranbd2017@outlook.com>
2023-07-11 22:13:56 +0100

    hello window
    https://learnopengl.com/Getting-started/Hello-Window

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,63 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) { // input handler
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main(int, char**){
     std::cout << "Hello, from MathGL!\n";
 
     glfwInit();
+    // Make GLFW requires OpenGL version 3.3+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // initialize the window
     GLFWwindow* window = glfwCreateWindow(800, 600, "MathGL", NULL, NULL);
-    if (window == NULL)
-    {
+    if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
+    // initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }  
-  
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // basically to handle window resizing
+
+    while(!glfwWindowShouldClose(window))
+    {
+        /// ---= INPUT =---
+        processInput(window);
+
+
+        /// ---= RENDERING =---
+        // bg color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        /// ---= STUFF =---
+        // swap buffers
+        glfwSwapBuffers(window);
+        // idk
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
